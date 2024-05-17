afc543068f7ada7bcb6ffcbaed6cea37e648599b
Sebastian Pytka <76405637+Fablek@users.noreply.github.com>
2024-05-17 22:29:38 +0200

    Dodaj pliki projektów.

---
+++ Szkoła/Szkoła.cpp
@@ -0,0 +1,39 @@
+﻿#include <stdlib.h>
+#include <iostream>
+#include "mysql_connection.h"
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/prepared_statement.h>
+using namespace std;
+
+
+const string server = "tcp://127.0.0.1:3306";
+const string username = "root";
+const string password = "";
+
+int main()
+{
+    sql::Driver* driver;
+    sql::Connection* con;
+    sql::Statement* stmt;
+    sql::PreparedStatement* pstmt;
+
+    try
+    {
+        driver = get_driver_instance();
+        con = driver->connect(server, username, password);
+    }
+    catch (sql::SQLException e)
+    {
+        cout << "Could not connect to server. Error message: " << e.what() << endl;
+        system("pause");
+        exit(1);
+    }
+
+    con->setSchema("testsql");
+
+    cout << "Połączono";
+
+    delete con;
+    return 0;
+}
